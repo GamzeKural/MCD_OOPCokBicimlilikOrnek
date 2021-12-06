b56d8754a514f3738bcb50a653d58988c427b816
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-06 16:16:35 +0300

    Add project files.

---
+++ MCD_OOPCokBicimlilikOrnek/Banka.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOPCokBicimlilikOrnek
+{
+    public class Banka
+    {
+
+        public string BankaAdi { get; set; }
+
+        private decimal _alisverisTutari=0;
+
+        public decimal AlisverisTutari
+        {
+            get;
+            set;
+        }
+
+        public void BilgileriAl()
+        {
+            Console.WriteLine(BankaAdi+" bankasına hoşgeldiniz...");
+            Console.WriteLine("Alışveriş tutarı giriniz: ");
+            _alisverisTutari = Convert.ToDecimal(Console.ReadLine());
+            if (_alisverisTutari>0)
+            {
+                AlisverisTutari = _alisverisTutari;
+            }
+            else
+            {
+                throw new Exception("Tutar sıfırdan büyük olmalıdır.");
+            }
+        }
+
+        public virtual void IndirimOraninaGoreIndirimYap()
+        {
+            AlisverisTutari -= AlisverisTutari * 0.20m;
+            Console.WriteLine("%20 indirim uygulandı...");
+            Console.WriteLine("Ödenecek tutar: "+AlisverisTutari);
+        }
+
+    }
+}
---
+++ MCD_OOPCokBicimlilikOrnek/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOPCokBicimlilikOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //polymorphism
+
+            Console.WriteLine("Banka seçiniz A/B : ");
+            ConsoleKeyInfo secim;
+            secim = Console.ReadKey();
+
+            if (secim.Key==ConsoleKey.A)
+            {
+                ABankasi bankam = new ABankasi();
+                bankam.BilgileriAl();
+                bankam.IndirimOraninaGoreIndirimYap();
+            }
+            else if (secim.Key==ConsoleKey.B)
+            {
+                BBankasi bankam = new BBankasi();
+                bankam.BilgileriAl();
+                bankam.IndirimOraninaGoreIndirimYap();
+            }
+            else
+            {
+                Console.WriteLine("Lütfen geçerli bir değer giriniz.");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
